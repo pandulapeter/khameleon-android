60cbfb9005878bdc485def28155b267a07d247ad
Péter Pandula <peter@halcyonmobile.com>
2018-03-03 13:42:56 +0200

    Display version number in Settings.

--- app/src/main/java/com/pandulapeter/khameleon/feature/home/settings/SettingsViewModel.kt
+++ app/src/main/java/com/pandulapeter/khameleon/feature/home/settings/SettingsViewModel.kt
@@ -1,5 +1,9 @@
 package com.pandulapeter.khameleon.feature.home.settings
 
+import com.pandulapeter.khameleon.BuildConfig
 import com.pandulapeter.khameleon.feature.KhameleonViewModel
 
-class SettingsViewModel : KhameleonViewModel()
+class SettingsViewModel : KhameleonViewModel() {
+
+    val version = BuildConfig.VERSION_NAME
+}

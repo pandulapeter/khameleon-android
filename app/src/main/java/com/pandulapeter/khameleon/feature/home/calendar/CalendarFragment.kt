bad07bf99de58254c019ad03cb06c1ad9f35abf6
Péter Pandula <peter@halcyonmobile.com>
2018-03-03 22:23:03 +0200

    Calendar improvemenrts.

--- app/src/main/java/com/pandulapeter/khameleon/feature/home/calendar/CalendarFragment.kt
+++ app/src/main/java/com/pandulapeter/khameleon/feature/home/calendar/CalendarFragment.kt
@@ -1,10 +1,22 @@
 package com.pandulapeter.khameleon.feature.home.calendar
 
+import android.os.Bundle
+import android.view.View
 import com.pandulapeter.khameleon.CalendarFragmentBinding
 import com.pandulapeter.khameleon.R
 import com.pandulapeter.khameleon.feature.KhameleonFragment
+import java.util.*
 
 class CalendarFragment : KhameleonFragment<CalendarFragmentBinding, CalendarViewModel>(R.layout.fragment_calendar) {
 
     override val viewModel = CalendarViewModel()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.calendarView.setMinimumDate(Calendar.getInstance().apply {
+            set(Calendar.YEAR, 2018)
+            set(Calendar.MONTH, 2)
+            set(Calendar.DAY_OF_MONTH, 1)
+        })
+        binding.calendarView.showCurrentMonthPage()
+    }
 }

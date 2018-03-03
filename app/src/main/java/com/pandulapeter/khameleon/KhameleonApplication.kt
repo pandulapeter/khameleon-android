671d8625a93da23e2e84a59adce663089d596b06
Péter Pandula <peter@halcyonmobile.com>
2018-03-03 13:53:47 +0200

    Enable offline cache.

--- app/src/main/java/com/pandulapeter/khameleon/KhameleonApplication.kt
+++ app/src/main/java/com/pandulapeter/khameleon/KhameleonApplication.kt
@@ -1,6 +1,7 @@
 package com.pandulapeter.khameleon
 
 import android.app.Application
+import com.google.firebase.database.FirebaseDatabase
 import com.pandulapeter.khameleon.injection.chatModule
 import com.pandulapeter.khameleon.injection.userModule
 import org.koin.android.ext.android.startKoin
@@ -9,6 +10,7 @@ class KhameleonApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true)
         startKoin(this, listOf(userModule, chatModule))
     }
 }

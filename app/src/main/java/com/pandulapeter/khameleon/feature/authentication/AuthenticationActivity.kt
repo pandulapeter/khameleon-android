9112678ded2b2f6e7b7bb5d8c649d4a43bef99df
Péter Pandula <peter@halcyonmobile.com>
2018-03-03 21:19:41 +0200

    Improve the authentication layout.

--- app/src/main/java/com/pandulapeter/khameleon/feature/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/pandulapeter/khameleon/feature/authentication/AuthenticationActivity.kt
@@ -11,6 +11,7 @@ import com.pandulapeter.khameleon.data.repository.MessageRepository
 import com.pandulapeter.khameleon.data.repository.UserRepository
 import com.pandulapeter.khameleon.feature.home.HomeActivity
 import com.pandulapeter.khameleon.feature.shared.KhameleonActivity
+import com.pandulapeter.khameleon.util.GlideApp
 import com.pandulapeter.khameleon.util.showSnackbar
 import org.koin.android.ext.android.inject
 
@@ -25,13 +26,15 @@ class AuthenticationActivity : KhameleonActivity<AuthenticationActivityBinding>(
     private val messageRepository by inject<MessageRepository>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(R.style.AppTheme_Authentication)
+        setTheme(R.style.Authentication)
         super.onCreate(savedInstanceState)
         if (userRepository.getSignedInUser() == null) {
-            setTitle(R.string.authentication)
             messageRepository.setPushNotificationsEnabled(false)
             binding.signInButton.setSize(SignInButton.SIZE_WIDE)
             binding.signInButton.setOnClickListener { startActivityForResult(userRepository.getSignInIntent(), AUTHENTICATION_REQUEST) }
+            GlideApp.with(this)
+                .load("https://scontent.fomr1-1.fna.fbcdn.net/v/t31.0-8/27164944_1597705330318762_2684267793485833634_o.jpg?oh=391d11915c86415c14de1a810db30c8b&oe=5B00B774")
+                .into(binding.logo)
         } else {
             startHomeScreen()
         }

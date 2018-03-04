b47a6f95b39968faf09281328751fcbc3b8794be
Péter Pandula <peter@halcyonmobile.com>
2018-03-04 13:25:12 +0200

    Replace Firebase Crash Reporting with Crashlytics.

--- app/src/main/java/com/pandulapeter/khameleon/KhameleonApplication.kt
+++ app/src/main/java/com/pandulapeter/khameleon/KhameleonApplication.kt
@@ -1,20 +1,23 @@
 package com.pandulapeter.khameleon
 
 import android.app.Application
-import com.google.firebase.crash.FirebaseCrash
+import com.crashlytics.android.Crashlytics
 import com.google.firebase.database.FirebaseDatabase
 import com.pandulapeter.khameleon.injection.calendarModule
 import com.pandulapeter.khameleon.injection.chatModule
 import com.pandulapeter.khameleon.injection.songsModule
 import com.pandulapeter.khameleon.injection.userModule
+import io.fabric.sdk.android.Fabric
 import org.koin.android.ext.android.startKoin
 
 class KhameleonApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        if (!BuildConfig.DEBUG) {
+            Fabric.with(this, Crashlytics())
+        }
         FirebaseDatabase.getInstance().setPersistenceEnabled(true)
-        FirebaseCrash.setCrashCollectionEnabled(!BuildConfig.DEBUG)
         startKoin(this, listOf(userModule, chatModule, calendarModule, songsModule))
     }
 }
